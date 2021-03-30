d595e2fd814ef0cae0da7e4d838bc9150b7e5b88
Isaac <72523606+missgorl@users.noreply.github.com>
2021-03-29 19:19:28 -0500

    Added API fetch requests
    The submission fetch works, however the result fetch is returning errors

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+"use strict";
+
+// Define API key
+const APIKEY = process.env.APIKEY
+
+
 // Dotenv Config
 const dotenv = require('dotenv');
 dotenv.config();
@@ -45,6 +51,25 @@ function findUrls( text )
     return urlArray;
 }
 
+//Embed function
+
+function embed(data, channel) {
+    const exampleEmbed = new Discord.MessageEmbed()
+    .setColor('#5863f8')
+    .setTitle('URL Report')
+    .setDescription('A detailed report on the safety of your URL')
+    .addField('\u200b', '\u200b')
+    .addFields(
+        
+        { name: 'Maliciousness score: (0-100) (lower is safer)', value: `${data.verdicts.urlscan.score}` }
+    )
+    .setImage(`https://urlscan.io/screenshots/${data.task.uuid}.png`)
+    .setTimestamp()
+    .setFooter('Powered by urlscan.io', 'https://i.imgur.com/GKxI8lY.png');
+
+    channel.send(exampleEmbed);
+}
+
 // Message listener
 
 client.on('message', message => {
@@ -54,6 +79,28 @@ client.on('message', message => {
     else if (message.content.includes('https://')) {
         message.channel.send('Link detected! Beginning Scan...');
         var messageLink = (findUrls(message.content))
-        message.channel.send("`" + "Your parsed URL: " + messageLink + "`")
-    }
-});
+        
+        fetch("https://urlscan.io/api/v1/scan/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "API-Key": `${APIKEY}`
+            },
+            body: JSON.stringify({
+                url: `${messageLink}`,
+                visibility: "public"
+            })
+        }).then(response => response.json()).then((data) => {
+            message.channel.send('URL submitted!')
+
+            message.channel.send('Retrieving scan data...')
+
+            setTimeout(function() {
+                fetch(`https://urlscan.io/api/v1/result/${data.uuid}/`).then(response => response.json()).then(data => embed(data, message.channel))
+            }, 20000);
+
+    
+        })
+                
+            }
+        });
---
+++ test.js
@@ -0,0 +1,19 @@
+
+// //Fetch config
+// const fetch = require('node-fetch');
+
+// fetch('https://urlscan.io/api/v1/scan/', {
+//     method: 'post',
+//     headers: {
+//         "Content-Type" : "application/json",
+//         "API-Key" : "679cc4e9-22a4-4c8e-8eb1-6d2656157a2b"
+//     },
+
+//     body: JSON.stringify({
+//          'url': 'https://plvr.xyz',
+//          'visibility': 'public',
+//          'tags': ['malware', 'phishing']  
+//     })
+// })
+// .then( (response) => response.json )
+// .then(data => console.log(data))
